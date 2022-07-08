0ec8cb31c8bafe1d252dc36ec38308140e9f5d66
Jonathan Whalley <jwhalley@Jonathans-MacBook-Pro.local>
2022-07-07 22:54:41 -0400

    Added CORS Middleware to Lab controller

--- lab_controller/main.py
+++ lab_controller/main.py
@@ -2,6 +2,7 @@
 
 import validators
 from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from . import models, schemas, crud
@@ -11,6 +12,16 @@ from .puzzlehelper import return_status_text
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
     db = SessionLocal()
